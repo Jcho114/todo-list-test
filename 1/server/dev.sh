ce8377f6f3ab73931adff9bd3c795a673d49d43e
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2024-08-25 18:47:46 -0400

    minified tailwind build file

--- 1/server/dev.sh
+++ 1/server/dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting tailwind cli"
-npx tailwindcss -i ./modules/templates/input.css -o ./modules/templates/public/output.css --watch
+npx tailwindcss -i ./modules/templates/input.css -o ./modules/templates/public/output.css --watch --minify
